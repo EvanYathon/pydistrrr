ac3178e55896266a00fb75d3860538f2bb6491e2
evanythn <evan.ythn@gmail.com>
2019-02-12 20:54:12 -0800

    removed placeholder code for distance functions - now using Mikes

--- pydistrrr/get_all_distances.py
+++ pydistrrr/get_all_distances.py
@@ -58,38 +58,6 @@ def get_all_distances(point, data, dist = "euclidean"):
     if not dist in ["euclidean","cosine","manhattan"]:
         raise Exception("dist should be one of 'euclidean','cosine' or 'manhattan'")
 
-    # define distance metric function
-    # placeholder, as the function will use get_distance
-    # def get_distance(point1, point2, metric = "euclidean"):
-    #     if metric == "euclidean":
-    #         ph = 0
-    #         for i in range(0,len(point1)):
-    #             ph += (point1[i]-point2[i])**2
-    #
-    #         return ph**(1/2)
-    #
-    #     elif metric == "cosine":
-    #         dp = 0
-    #         amag = 0
-    #         bmag = 0
-    #
-    #         for i in range(0,len(point1)):
-    #             dp += point1[i]*point2[i]
-    #             amag += point1[i]**2
-    #             bmag += point2[i]**2
-    #
-    #         amag = amag**(1/2)
-    #         bmag = bmag**(1/2)
-    #
-    #         return dp/(amag*bmag)
-    #
-    #     elif metric == "manhattan":
-    #         ph = 0
-    #         for i in range(0,len(point1)):
-    #             ph += abs(point1[i]-point2[i])
-    #
-    #         return ph
-
     # empty vector to be filled with distances
     distances = []
 
