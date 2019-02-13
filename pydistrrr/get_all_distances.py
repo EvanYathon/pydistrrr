35b9dd0c40929842e53fd10380e8e30f917f4eab
evanythn <evan.ythn@gmail.com>
2019-02-12 20:46:45 -0800

    changed function to use get_distance created by Mike

--- pydistrrr/get_all_distances.py
+++ pydistrrr/get_all_distances.py
@@ -34,8 +34,9 @@ def get_all_distances(point, data, dist = "euclidean"):
     point = [-2,4]
     get_all_distances(point, df, dist = "euclidean")
     """
-    # import pandas
+    # import pandas and get_all_distances
     import pandas as pd
+    from pydistrrr.get_distance import get_distance
 
     # raise error if dataframe isn't the correct type of object
     if not isinstance(data, pd.DataFrame):
@@ -59,35 +60,35 @@ def get_all_distances(point, data, dist = "euclidean"):
 
     # define distance metric function
     # placeholder, as the function will use get_distance
-    def get_distance(point1, point2, metric = "euclidean"):
-        if metric == "euclidean":
-            ph = 0
-            for i in range(0,len(point1)):
-                ph += (point1[i]-point2[i])**2
-
-            return ph**(1/2)
-
-        elif metric == "cosine":
-            dp = 0
-            amag = 0
-            bmag = 0
-
-            for i in range(0,len(point1)):
-                dp += point1[i]*point2[i]
-                amag += point1[i]**2
-                bmag += point2[i]**2
-
-            amag = amag**(1/2)
-            bmag = bmag**(1/2)
-
-            return dp/(amag*bmag)
-
-        elif metric == "manhattan":
-            ph = 0
-            for i in range(0,len(point1)):
-                ph += abs(point1[i]-point2[i])
-
-            return ph
+    # def get_distance(point1, point2, metric = "euclidean"):
+    #     if metric == "euclidean":
+    #         ph = 0
+    #         for i in range(0,len(point1)):
+    #             ph += (point1[i]-point2[i])**2
+    #
+    #         return ph**(1/2)
+    #
+    #     elif metric == "cosine":
+    #         dp = 0
+    #         amag = 0
+    #         bmag = 0
+    #
+    #         for i in range(0,len(point1)):
+    #             dp += point1[i]*point2[i]
+    #             amag += point1[i]**2
+    #             bmag += point2[i]**2
+    #
+    #         amag = amag**(1/2)
+    #         bmag = bmag**(1/2)
+    #
+    #         return dp/(amag*bmag)
+    #
+    #     elif metric == "manhattan":
+    #         ph = 0
+    #         for i in range(0,len(point1)):
+    #             ph += abs(point1[i]-point2[i])
+    #
+    #         return ph
 
     # empty vector to be filled with distances
     distances = []
