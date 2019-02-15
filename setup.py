c26ff19c3c0ee1b290e2f63c47d873943974f6b5
evanythn <evan.ythn@gmail.com>
2019-02-15 09:26:34 -0800

    added setup code to setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pydistrrr',
+      version='1.0',
+      description='Distance Metrics',
+      author='Shayne Andrews, Carrie Cheung, Evan Yathon, Mike Yuan',
+      url='https://github.com/UBC-MDS/pydistrrr',
+      install_requires=[
+      'pandas',
+      'numpy']
+     )
