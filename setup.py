374ca4927e770e31336a51e697d4acc9e301ecc5
Mike Yuan <yuan.mike84@gmail.com>
2019-03-03 11:47:27 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pydistrrr",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A package calculate distnaces between points",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/UBC-MDS/pydistrrr",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
-=======
 from distutils.core import setup
 
 setup(name='pydistrrr',
@@ -29,5 +6,4 @@ setup(name='pydistrrr',
       author='Shayne Andrews, Carrie Cheung, Evan Yathon, Mike Yuan',
       url='https://github.com/UBC-MDS/pydistrrr',
       packages=['pydistrrr']
-     )
->>>>>>> upstream/master
+      )
